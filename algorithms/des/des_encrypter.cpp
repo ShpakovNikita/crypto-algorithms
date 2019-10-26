cc318e9c3622f34c1e89d6f6d87af6a8cd94f90e
shpakov_nikita <shpakovnikita1998@gmail.com>
2019-10-26 19:22:49 +0300

    substitution boxes added

--- algorithms/des/des_encrypter.cpp
+++ algorithms/des/des_encrypter.cpp
@@ -76,6 +76,76 @@ const std::vector<uint8_t> P =
 // matrix that determine the shift for each round of keys
 const std::vector<uint8_t> SHIFT = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
 
+const std::vector<std::vector<uint8_t>> S_BOX_1 =
+{
+	{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+	{0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+	{4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
+	{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13},
+};
+
+const std::vector<std::vector<uint8_t>> S_BOX_2 =
+{
+	{15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+	{3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
+	{0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
+	{13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9},
+};
+
+const std::vector<std::vector<uint8_t>> S_BOX_3 =
+{
+	{10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+	{13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
+	{13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
+	{1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12},
+};
+
+const std::vector<std::vector<uint8_t>> S_BOX_4 =
+{
+	{7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+	{13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+	{10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
+	{3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14},
+};
+
+const std::vector<std::vector<uint8_t>> S_BOX_5 =
+{
+	{2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+	{14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
+	{4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
+	{11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3},
+};
+
+const std::vector<std::vector<uint8_t>> S_BOX_6 =
+{
+	{12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+	{10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
+	{9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
+	{4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13},
+};
+
+const std::vector<std::vector<uint8_t>> S_BOX_7 =
+{
+	{4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
+	{13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
+	{1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
+	{6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12},
+};
+
+const std::vector<std::vector<uint8_t>> S_BOX_8 =
+{
+	{13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+	{1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
+	{7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
+	{2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11},
+};
+
+const std::vector<std::vector<std::vector<uint8_t>>> S_BOX =
+{
+	S_BOX_1, S_BOX_2, S_BOX_3, S_BOX_4, S_BOX_5, S_BOX_6, S_BOX_7, S_BOX_8,
+};
+
+
 namespace _bit_utils
 {
 	template <uint32_t initial_bytes_count, uint32_t output_bytes_count>
@@ -136,20 +206,21 @@ namespace _bit_utils
 		return bitset;
 	}
 
-	template <uint32_t bytes_count>
-	static std::tuple<std::bitset<bytes_count * CHAR_BIT / 2>, 
-		std::bitset<bytes_count* CHAR_BIT / 2>> split_bitset(
+	template <uint32_t bytes_count, uint32_t slices_count = 2>
+	static std::vector<std::bitset<bytes_count * CHAR_BIT / slices_count>> split_bitset(
 			const std::bitset<bytes_count * CHAR_BIT>& data)
 	{
-		constexpr uint64_t split_size = bytes_count * CHAR_BIT / 2;
-		std::bitset<split_size> left, right;
+		constexpr uint64_t split_size = bytes_count * CHAR_BIT / slices_count;
+		std::vector<std::bitset<split_size>> slices;
+		// TODO: slices.reserve()
+
 		for (uint64_t i = 0; i < split_size; ++i)
 		{
 			left[i] = data[i];
 			right[i] = data[i + split_size];
 		}
 
-		return std::make_tuple(left, right);
+		return { left, right };
 	}
 
 	template <uint32_t bytes_count>
@@ -244,7 +315,8 @@ void des_encrypter::_generate_keys()
 
 	auto key = _bit_utils::bytes_to_bitset<BLOCK_SIZE>(_bit_utils::stob(_key));
 	auto permutated_key = _bit_utils::perform_permutations<BLOCK_SIZE, (BLOCK_SIZE - 1)>(key, CP_1);
-	auto [left, right] = _bit_utils::split_bitset<(BLOCK_SIZE - 1)>(permutated_key);
+	auto pair = _bit_utils::split_bitset<(BLOCK_SIZE - 1)>(permutated_key);
+	auto left = pair[0], right = pair[1];
 
 	for (uint64_t i = 0; i < ROUNDS_COUNT; ++i)
 	{
@@ -286,7 +358,8 @@ std::bitset<BLOCK_SIZE* CHAR_BIT> des_encrypter::_encrypt_block(const std::strin
 	auto bitset_block = _bit_utils::bytes_to_bitset<BLOCK_SIZE>(_bit_utils::stob(block));
 	auto permutated_block = _bit_utils::perform_permutations<BLOCK_SIZE, BLOCK_SIZE>(bitset_block, PI);
 
-	auto& [left, right] = _bit_utils::split_bitset<BLOCK_SIZE>(permutated_block);
+	auto pair = _bit_utils::split_bitset<BLOCK_SIZE>(permutated_block);
+	auto left = pair[0], right = pair[1];
 
 	for (uint64_t i = 0; i < ROUNDS_COUNT; ++i)
 	{
@@ -321,6 +394,13 @@ std::bitset<BLOCK_SIZE* CHAR_BIT> des_encrypter::_encrypt_block(const std::strin
 	auto result_block = _bit_utils::perform_permutations<BLOCK_SIZE, BLOCK_SIZE>(bitset_block, PI_1);
 
 	return result_block;
+}
+
+std::bitset<BLOCK_SIZE* CHAR_BIT> des_encrypter::_substitude_block(const std::string& block)
+{
+
+
+	return {};
 }
 
 const char* des_encrypter::invalid_key::what() const throw ()
--- algorithms/des/des_encrypter.hpp
+++ algorithms/des/des_encrypter.hpp
@@ -40,6 +40,7 @@ private:
 	std::string _internal_run(const std::string& message, _e_action action);
 
 	std::bitset<BLOCK_SIZE * CHAR_BIT> _encrypt_block(const std::string& block, _e_action action);
+	std::bitset<BLOCK_SIZE* CHAR_BIT> _substitude_block(const std::string& block);
 
 	std::string _key;
 	std::vector<std::bitset<(BLOCK_SIZE - 2) * CHAR_BIT>> _generated_keys;
