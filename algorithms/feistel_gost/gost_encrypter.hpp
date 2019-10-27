1f71ae8dc4f0cb5b72b8d57080f42cb16c4ee135
shpakov_nikita <shpakovnikita1998@gmail.com>
2019-10-28 00:04:49 +0300

    gost algo project added

---
+++ algorithms/feistel_gost/gost_encrypter.hpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <bitset>
+
+constexpr uint32_t BLOCK_SIZE = 8;
+
+class gost_encrypter
+{
+public:
+	struct invalid_key : public std::exception
+	{
+		const char* what() const throw ();
+	};
+
+	gost_encrypter(const std::string& key);
+	~gost_encrypter() = default;
+
+	std::string encrypt(const std::string& message) const;
+	std::string decrypt(const std::string& message) const;
+
+private:
+	struct invalid_action : public std::exception
+	{
+		const char* what() const throw ();
+	};
+
+	enum class _e_action
+	{
+		encrypt = 0,
+		decrypt,
+		undefined,
+	};
+
+	static std::vector<std::string> _build_message_blocks(const std::string& message);
+	static std::string _try_remove_padding(const std::string& message);
+	static std::string _check_key(const std::string& key);
+	static std::string _construct_padding_message(const std::string& message);
+
+	static std::bitset<BLOCK_SIZE* CHAR_BIT / 2> _substitude_block(const std::bitset<(BLOCK_SIZE - 2)* CHAR_BIT>& block);
+
+	std::bitset<BLOCK_SIZE* CHAR_BIT> _encrypt_block(const std::string& block, _e_action action) const;
+	std::string _internal_run(const std::string& message, _e_action action) const;
+
+	void _generate_keys();
+
+	std::string _key;
+	std::vector<std::bitset<(BLOCK_SIZE - 2) * CHAR_BIT>> _generated_keys;
+};
---
+++ algorithms/feistel_gost/main.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <cassert>
+
+#include "gost_encrypter.hpp"
+
+static uint32_t tests_passed = 0;
+
+#define TEST_CASE_BEGIN(case_name) \
+void case_name() \
+{ \
+	std::cerr << #case_name << " test started!" << std::endl;
+
+#define TEST_CASE_END() \
+	std::cerr << __FUNCTION__ << " test passed!\n" << std::endl; \
+	++tests_passed; \
+}
+
+TEST_CASE_BEGIN(cypher_base_encrypt_decrypt)
+{
+	std::string message = "Hello wo";
+	std::string key = "secret_k";
+
+	gost_encrypter encrypter(key);
+	std::cout << "initial name " << message << std::endl;
+
+	std::string encrypted = encrypter.encrypt(message);
+	std::cout << "encrypted name " << encrypted << std::endl;
+
+	assert(message != encrypted);
+
+	std::string decrypted = encrypter.decrypt(encrypted);
+	std::cout << "decrypted name " << decrypted << std::endl;
+
+	assert(message == decrypted);
+}
+TEST_CASE_END()
+
+TEST_CASE_BEGIN(cypher_long_message_encrypt_decrypt)
+{
+	std::string message = "Hello wssdd#2123o";
+	std::string key = "secret_s";
+
+	gost_encrypter encrypter(key);
+	std::cout << "initial name " << message << std::endl;
+
+	std::string encrypted = encrypter.encrypt(message);
+	std::cout << "encrypted name " << encrypted << std::endl;
+
+	assert(message != encrypted);
+
+	std::string decrypted = encrypter.decrypt(encrypted);
+	std::cout << "decrypted name " << decrypted << std::endl;
+
+	assert(message == decrypted);
+}
+TEST_CASE_END()
+
+int main()
+{
+	try
+	{
+		cypher_base_encrypt_decrypt();
+		cypher_long_message_encrypt_decrypt();
+
+		std::cerr << tests_passed << " tests passed!" << std::endl;
+	}
+	catch (const gost_encrypter::invalid_key & e)
+	{
+		std::cerr << tests_passed << " tests passed, before error occurred" << std::endl;
+
+		std::cerr << e.what() << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
